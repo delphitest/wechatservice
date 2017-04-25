7cfcf74e3408f1893bc2443ca408f09b083b0209
terrylu87 <terrylu87@gmail.com>
2017-04-26 00:34:19 +0800

    a

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ class wechatCallbackapiTest
 {  
     public function valid()//验证接口的方法  
     {  
+        $echoStr = "";
         $echoStr = $_GET["echostr"];//从微信用户端获取一个随机字符赋予变量echostr  
   
         //valid signature , option访问地61行的checkSignature签名验证方法，如果签名一致，输出变量echostr，完整验证配置接口的操作  
@@ -66,6 +67,9 @@ class wechatCallbackapiTest
     //签名验证程序    ，checkSignature被18行调用。官方加密、校验流程：将token，timestamp，nonce这三个参数进行字典序排序，然后将这三个参数字符串拼接成一个字符串惊喜shal加密，开发者获得加密后的字符串可以与signature对比，表示该请求来源于微信。  
     private function checkSignature()  
     {  
+        $signature = "";  
+        $timestamp = "";  
+        $nonce = ""; 
         $signature = $_GET["signature"];//从用户端获取签名赋予变量signature  
         $timestamp = $_GET["timestamp"];//从用户端获取时间戳赋予变量timestamp  
         $nonce = $_GET["nonce"];    //从用户端获取随机数赋予变量nonce  
