e8124d0a206dc8e4df8265cfd183bfc6a103330c
delphitest <jinchun.gao@delphi.com>
2017-04-26 09:58:05 +0800

    Update index.php
    a

--- index.php
+++ index.php
@@ -27,12 +27,10 @@ class wechatCallbackapiTest
         //extract post data如果用户端数据不为空，执行30-55否则56-58  
         if (!empty($postStr)){  
                   
-                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);//将postStr变量进行解析并赋予变量postObj。simplexml_load_string（）函数是php中一个解析XML的函数，SimpleXMLElement为新对象的类，LIBXML_NOCDATA表示将CDATA设置为文本节点，CDATA标签中的文本XML不进行解析  
-
                 $file = fopen("in_msg","w");
                 fwrite($file,$postStr);
                 fclose($file);
-                      
+                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);//将postStr变量进行解析并赋予变量postObj。simplexml_load_string（）函数是php中一个解析XML的函数，SimpleXMLElement为新对象的类，LIBXML_NOCDATA表示将CDATA设置为文本节点，CDATA标签中的文本XML不进行解析  
                 $fromUsername = $postObj->FromUserName;//将微信用户端的用户名赋予变量FromUserName  
                 $toUsername = $postObj->ToUserName;//将你的微信公众账号ID赋予变量ToUserName  
                 $keyword = trim($postObj->Content);//将用户微信发来的文本内容去掉空格后赋予变量keyword  
